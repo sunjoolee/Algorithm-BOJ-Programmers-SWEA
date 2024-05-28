eb55bf09d699e4a62a367f7c852c708eb4cc101d
sunjoolee <86330886+sunjoolee@users.noreply.github.com>
2024-05-28 22:54:26 +0900

    [level 2] Title: H-Index, Time: 20.23 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.kt
+++ 프로그래머스/2/42747. H－Index/H－Index.kt
@@ -1,22 +1,16 @@
-import kotlin.math.*
-import java.util.*
-
 class Solution {
     fun solution(citations: IntArray): Int {
+        val sortedCitations = citations.toList().sorted() 
         
-        var answer = 0
-        
-        val maxH = citations.toList().maxOrNull() ?: 1000
-        val minH = 0
-        for(h in maxH downTo minH){
-            //h번 이상 인용된 논문의 수
-            var hCnt = citations.toList().filter{it >= h}.size
+        val n = sortedCitations.size 
+        var h = sortedCitations.last() 
+        while(h >= 0){
+            val moreThanHCnt = n - sortedCitations.indexOfFirst{it >= h} 
+            val restCnt = n - moreThanHCnt 
             
-            if(hCnt >= h){
-                answer = h
-                break
-            }
+            if((moreThanHCnt >= h) && (h >= restCnt)) return h
+            h--
         }
-        return answer
+        return -1 //not reached
     }
 }
